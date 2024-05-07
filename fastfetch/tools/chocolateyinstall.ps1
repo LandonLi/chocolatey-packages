7193f2f26c7771e52f85fb1deda3443715d24b97
Landon Li <lxx4work@gmail.com>
2024-05-07 20:37:05 +0800

    fastfetch: Update to 2.11.5

---
+++ fastfetch/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/fastfetch-cli/fastfetch/releases/latest/download/fastfetch-windows-amd64.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  softwareName  = 'fastfetch*'
+  checksum      = '4B54AB8182E51E53D7D138CA12EBFE5CA8B431C394AFE4877DEC2B301271BB21'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
---
+++ fastfetch/update.ps1
@@ -0,0 +1,22 @@
+Import-Module au
+
+$releases = "https://api.github.com/repos/fastfetch-cli/fastfetch/releases"
+$file = "fastfetch-windows-amd64.zip"
+
+function global:au_GetLatest {
+    $latestRelease = ((Invoke-WebRequest -Uri $releases -UseBasicParsing -Proxy $env:HTTP_PROXY).Content | ConvertFrom-Json)[0]
+    $version = $latestRelease.name
+    $pattern = "([0-9a-z]{64})\s+.+\/$file"
+    $checkSum = (($latestRelease.body | Select-String -Pattern $pattern).Matches.Value -split "\s+")[0].ToUpper()
+    return @{ Version = $version; Checksum = $checkSum }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+            "(\s+checksum\s+=\s+)('.*')" = "`$1'$($Latest.Checksum)'"
+        }      
+    }
+}
+
+update -ChecksumFor none
