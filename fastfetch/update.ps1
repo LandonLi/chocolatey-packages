fd58c8da94eb813ec655894243947ad07023a51e
Landon Li <lxx4work@gmail.com>
2024-05-15 20:29:44 +0800

    CI: update fastfetch.yaml

--- fastfetch/update.ps1
+++ fastfetch/update.ps1
@@ -1,10 +1,10 @@
 Import-Module au
 
-$releases = "https://api.github.com/repos/fastfetch-cli/fastfetch/releases"
+$releases = "https://api.github.com/repos/fastfetch-cli/fastfetch/releases/latest"
 $file = "fastfetch-windows-amd64.zip"
 
 function global:au_GetLatest {
-    $latestRelease = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Content | ConvertFrom-Json)[0]
+    $latestRelease = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Content | ConvertFrom-Json)
     $version = $latestRelease.name
     $pattern = "([0-9a-z]{64})\s+.+\/$file"
     $checkSum = (($latestRelease.body | Select-String -Pattern $pattern).Matches.Value -split "\s+")[0].ToUpper()
