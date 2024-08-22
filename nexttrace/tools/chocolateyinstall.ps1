929a9df54ea3e2fd74602ed7236b82f84a280dca
LandonLi <58315416+LandonLi@users.noreply.github.com>
2024-08-22 11:57:49 +0000

    nexttrace: Update to v1.3.2

--- nexttrace/tools/chocolateyinstall.ps1
+++ nexttrace/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url64 = 'https://github.com/nxtrace/NTrace-core/releases/download/v1.3.1/nexttrace_windows_amd64.exe'
-$Checksum64 = '300196abca057d1dac7129c019519fd2582af4f4d18a482b8a32fa78f0081ae4'
+$url64 = 'https://gh.landon.li/https://github.com/nxtrace/NTrace-core/releases/download/v1.3.2/nexttrace_windows_amd64.exe'
+$Checksum64 = '2b720847044c43f7a4045096959c7c12747e32e45d97f9aae892a9f359fc6da9'
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
