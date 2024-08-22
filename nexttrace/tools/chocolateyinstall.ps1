24b1ff09a86254a33882a583eed56f2796bd9db5
Landon Li <lxx4work@gmail.com>
2024-08-22 19:55:14 +0800

    nexttrace: Update to 1.3.1

---
+++ nexttrace/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$url64 = 'https://github.com/nxtrace/NTrace-core/releases/download/v1.3.1/nexttrace_windows_amd64.exe'
+$Checksum64 = '300196abca057d1dac7129c019519fd2582af4f4d18a482b8a32fa78f0081ae4'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$fileArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileFullPath   = "$toolsDir\$env:ChocolateyPackageName.exe"
+  url64bit       = $url64
+  checksum64     = $Checksum64
+  checksumType64 = "sha256"
+}
+
+$localFileArgs = @{
+  name = $env:ChocolateyPackageName
+  path = $toolsDir
+}
+
+Get-ChocolateyWebFile @fileArgs
+Install-BinFile @localFileArgs
---
+++ nexttrace/update.ps1
@@ -0,0 +1,23 @@
+Import-Module au
+
+$releases = "https://api.github.com/repos/nxtrace/NTrace-core/releases/latest"
+
+function global:au_GetLatest {
+    $latestRelease = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Content | ConvertFrom-Json)
+    $version = $latestRelease.tag_name.TrimStart("v")
+    $url = ($latestRelease.assets | Where-Object -Property name -match "windows_amd64").browser_download_url
+    return @{ Version = $version; URL64 = $url }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+            "(^[$]url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+            "(^[$]Checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+    Write-Host "URL: $Latest.URL64`n"
+    Write-Host "CheckSum: $Latest.Checksum64`n"
+}
+
+update -ChecksumFor 64
