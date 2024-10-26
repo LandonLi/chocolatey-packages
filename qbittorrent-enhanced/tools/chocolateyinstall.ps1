ceb7a90ed2bf50d891e6c2fb60c10d1d8ee76b9f
Landon Li <lxx4work@gmail.com>
2024-10-26 23:03:08 +0800

    qbittorrent-enhanced(fix): Update the auto upgrade script

--- qbittorrent-enhanced/tools/chocolateyinstall.ps1
+++ qbittorrent-enhanced/tools/chocolateyinstall.ps1
@@ -1,20 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url32 = 'https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-4.6.7.10/qbittorrent_enhanced_4.6.7.10_setup.exe'
-$url64 = 'https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-4.6.7.10/qbittorrent_enhanced_4.6.7.10_x64_setup.exe'
-$Checksum32 = 'a92f43fa2e63c17828001d44085c32d8fb1723a7da66b57645c43456b110f84b'
-$Checksum64 = 'cd4642004f85338b9e97bb73935d8565f8ed529b2de17a1f0b3c4f9dd9bef7f8'
+$url = 'https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-4.6.7.10/qbittorrent_enhanced_4.6.7.10_x64_setup.exe'
+$Checksum = 'cd4642004f85338b9e97bb73935d8565f8ed529b2de17a1f0b3c4f9dd9bef7f8'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = "exe"
-  url            = $url32
-  url64bit       = $url64
+  url            = $url
+  url64bit       = $url
   silentArgs     = "/S"
   softwareName   = "qbittorrent*"
-  checksum       = $Checksum32
+  checksum       = $Checksum
   checksumType   = "sha256"
-  checksum64     = $Checksum64
+  checksum64     = $Checksum
   checksumType64 = "sha256"
 }
 
--- qbittorrent-enhanced/update.ps1
+++ qbittorrent-enhanced/update.ps1
@@ -6,20 +6,16 @@ function global:au_GetLatest {
     $latestRelease = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Content | ConvertFrom-Json)
     $version = $latestRelease.tag_name.Split("-")[1]
     Write-Host $version
-    $pattern32 = ".+qbittorrent_enhanced_${version}_setup.exe"
-    $pattern64 = ".+qbittorrent_enhanced_${version}_x64_setup.exe"
-    $url32 = ($latestRelease.assets | Where-Object browser_download_url -CMatch $pattern32)[0].browser_download_url
-    $url64 = ($latestRelease.assets | Where-Object browser_download_url -CMatch $pattern64)[0].browser_download_url
-    return @{ Version = $version; URL32 = $url32; URL64 = $url64 }
+    $pattern = ".+qbittorrent_enhanced_${version}_x64_setup.exe"
+    $url = ($latestRelease.assets | Where-Object browser_download_url -CMatch $pattern)[0].browser_download_url
+    return @{ Version = $version; URL32 = $url; URL64 = $url }
 }
 
 function global:au_SearchReplace {
     @{
         "tools\chocolateyinstall.ps1" = @{
-            "(^[$]url32\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
-            "(^[$]url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
-            "(^[$]Checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]Checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(^[$]url\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+            "(^[$]Checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
