ce3bff874dc30cc554a13d2df40d1314503c3f79
LandonLi <58315416+LandonLi@users.noreply.github.com>
2024-11-19 01:18:59 +0000

    qbittorrent-enhanced: Update to v5.0.2.10

--- qbittorrent-enhanced/tools/chocolateyinstall.ps1
+++ qbittorrent-enhanced/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url = 'https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-5.0.0.10/qbittorrent_enhanced_5.0.0.10_x64_setup.exe'
-$Checksum = '618e392f70ae56055d95556e027aa891b63f81899d364c83adb95854fd86f6a8'
+$url = 'https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-5.0.2.10/qbittorrent_enhanced_5.0.2.10_x64_setup.exe'
+$Checksum = '20269687c0bcef9097cf51ddad8db94887a4dee05749118ecbeecba0ee75801a'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
