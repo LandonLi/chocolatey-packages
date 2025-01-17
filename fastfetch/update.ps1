814ba164c346abf3d2cc65bdefc122deb4f2bc49
Landon Li <lxx4work@gmail.com>
2025-01-17 09:56:59 +0800

    fastfetch: Use the download url with version number

--- fastfetch/update.ps1
+++ fastfetch/update.ps1
@@ -6,14 +6,16 @@ $file = "fastfetch-windows-amd64.zip"
 function global:au_GetLatest {
     $latestRelease = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Content | ConvertFrom-Json)
     $version = $latestRelease.name
-    $pattern = "([0-9a-z]{64})\s+.+\/$file"
-    $checkSum = (($latestRelease.body | Select-String -Pattern $pattern).Matches.Value -split "\s+")[0].ToUpper()
-    return @{ Version = $version; Checksum = $checkSum }
+    $checkSumPattern = "([0-9a-z]{64})\s+.+\/$file"
+    $checkSum = (($latestRelease.body | Select-String -Pattern $checkSumPattern).Matches.Value -split "\s+")[0].ToUpper()
+    $url = ($latestRelease.assets | Where-Object browser_download_url -CMatch ".*fastfetch-windows-amd64.zip")[0].browser_download_url
+    return @{ Version = $version; URL32 = $url; URL64 = $url; Checksum = $checkSum }
 }
 
 function global:au_SearchReplace {
     @{
         "tools\chocolateyinstall.ps1" = @{
+            "(^[$]url\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
             "(\s+checksum\s+=\s+)('.*')" = "`$1'$($Latest.Checksum)'"
         }      
     }
