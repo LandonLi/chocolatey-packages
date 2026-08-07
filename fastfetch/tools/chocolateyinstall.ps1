4063fd304a40b1f5e16af528b915c9d51a50ddca
LandonLi <58315416+LandonLi@users.noreply.github.com>
2026-08-07 02:45:42 +0000

    fastfetch: Update to v2.67.0

--- fastfetch/tools/chocolateyinstall.ps1
+++ fastfetch/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/fastfetch-cli/fastfetch/releases/download/2.66.0/fastfetch-windows-amd64.zip'
+$url        = 'https://github.com/fastfetch-cli/fastfetch/releases/download/2.67.0/fastfetch-windows-amd64.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   url           = $url
   softwareName  = 'fastfetch*'
-  checksum      = '3035B8ADD360BC3142CE81AC39BE352D3738273224E232195217F01C2398C87A'
+  checksum      = '259E2F57C5ED09C5E1903032E16378056E9DD908DFF2FC4015AE74EA968A1B9D'
   checksumType  = 'sha256'
 }
 
